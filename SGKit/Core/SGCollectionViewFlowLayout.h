50cbeb43a125717a6a02700ad17a5aa5b5579c08
Single <musicman_leehom@126.com>
2016-11-05 19:02:54 +0800

    demo工程

---
+++ SGKit/Core/SGCollectionViewFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  SGCollectionViewFlowLayout.h
+//  SGKit
+//
+//  Created by Single on 2016/11/5.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGCollectionViewFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ SGKit/Core/SGCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  SGCollectionViewFlowLayout.m
+//  SGKit
+//
+//  Created by Single on 2016/11/5.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import "SGCollectionViewFlowLayout.h"
+
+@implementation SGCollectionViewFlowLayout
+
+@end
