e0593eb077bc428bfdc78c85eb0e9b9eb1318cc7
Single <musicman_leehom@126.com>
2016-11-05 18:45:36 +0800

    Initial Commit

---
+++ SGKit/SGKit.h
@@ -0,0 +1,19 @@
+//
+//  SGKit.h
+//  SGKit
+//
+//  Created by Single on 2016/11/5.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGKit.
+FOUNDATION_EXPORT double SGKitVersionNumber;
+
+//! Project version string for SGKit.
+FOUNDATION_EXPORT const unsigned char SGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGKit/PublicHeader.h>
+
+
