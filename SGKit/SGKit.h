113b3bda67b79eaaf12aabff43de13e346520bb6
Single <musicman_leehom@126.com>
2016-11-05 23:59:07 +0800

    加入UIColor类别

--- SGKit/SGKit.h
+++ SGKit/SGKit.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char SGKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SGKit/PublicHeader.h>
 #import <SGKit/SGCollectionViewFlowLayout.h>
+#import <SGKit/UIColor+Extsion.h>
 
