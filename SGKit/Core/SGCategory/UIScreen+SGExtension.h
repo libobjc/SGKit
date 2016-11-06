9341714a4541028cc636e5207b755f6dc93a59b7
Single <musicman_leehom@126.com>
2016-11-06 22:48:47 +0800

    完善UIScreen分类

--- SGKit/Core/SGCategory/UIScreen+SGExtension.h
+++ SGKit/Core/SGCategory/UIScreen+SGExtension.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+// 屏幕尺寸
+typedef NS_ENUM(NSUInteger, SGScreenSizeType)
+{
+    SGScreenSizeTypeUnknown,
+    SGScreenSizeType35,   // 3.5寸
+    SGScreenSizeType40,   // 4.0寸
+    SGScreenSizeType47,   // 4.7寸
+    SGScreenSizeType55,   // 5.5寸
+};
+
 @interface UIScreen (SGExtension)
 
-+ (UIImage *)snapshotForMainScreen;    // mainScreen截屏
++ (SGScreenSizeType)sg_screenSizeType;
++ (UIImage *)sg_snapshot;
+
++ (CGRect)sg_bounds;
++ (CGSize)sg_size;
++ (CGPoint)sg_origin;
++ (CGFloat)sg_x;
++ (CGFloat)sg_y;
++ (CGFloat)sg_width;
++ (CGFloat)sg_height;
 
 @end
--- SGKit/Core/SGCategory/UIScreen+SGExtension.m
+++ SGKit/Core/SGCategory/UIScreen+SGExtension.m
@@ -10,7 +10,31 @@
 
 @implementation UIScreen (SGExtension)
 
-+ (UIImage *)snapshotForMainScreen
++ (SGScreenSizeType)sg_screenSizeType
+{
+    if (![UIScreen instancesRespondToSelector:@selector(currentMode)]) return SGScreenSizeTypeUnknown;
+    
+    CGSize size = [UIScreen mainScreen].currentMode.size;
+    if (CGSizeEqualToSize(size, CGSizeMake(1242, 2208)))
+    {
+        return SGScreenSizeType55;
+    }
+    else if (CGSizeEqualToSize(size, CGSizeMake(750, 1334)))
+    {
+        return SGScreenSizeType47;
+    }
+    else if (CGSizeEqualToSize(size, CGSizeMake(640, 1136)))
+    {
+        return SGScreenSizeType40;
+    }
+    else if (CGSizeEqualToSize(size, CGSizeMake(640, 960)))
+    {
+        return SGScreenSizeType35;
+    }
+    return SGScreenSizeTypeUnknown;
+}
+
++ (UIImage *)sg_snapshot
 {
     CGSize imageSize = [[UIScreen mainScreen] bounds].size;
     UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
@@ -30,6 +54,41 @@
     UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return image;
+}
+
++ (CGRect)sg_bounds
+{
+    return [UIScreen mainScreen].bounds;
+}
+
++ (CGSize)sg_size
+{
+    return [self sg_bounds].size;
+}
+
++ (CGPoint)sg_origin
+{
+    return [self sg_bounds].origin;
+}
+
++ (CGFloat)sg_x
+{
+    return [self sg_origin].x;
+}
+
++ (CGFloat)sg_y
+{
+    return [self sg_origin].y;
+}
+
++ (CGFloat)sg_width
+{
+    return [self sg_size].width;
+}
+
++ (CGFloat)sg_height
+{
+    return [self sg_size].height;
 }
 
 @end
